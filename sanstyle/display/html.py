c24b33706aebb9be92d1c5f0a59494ee3f812658
xinetzone <xinzone@outlook.com>
2021-07-10 18:22:52 +0800

    +iframe

--- sanstyle/display/html.py
+++ sanstyle/display/html.py
@@ -1,17 +1,24 @@
-class Embed:
-    """
-    Generic class to embed an embed in an IPython notebook
-    """
+from string import Template
 
-    embed = \
-        """<embed typeName="{typeName}" width="{width}" height="{height}" src="{src} {params}">"""
+element_template = Template(
+    '''<${tag} class="${className}" type="${typeName}" ${params}>${content}</${tag}>''')
+null_element_template = Template(
+    '''<${tag} class="${className}" type="${typeName}" ${params}>''')
 
-    def __init__(self, typeName, src, width, height, **kwargs):
+
+class FrameElement:
+    def __init__(self, tag, src, height,
+                 width='100%',
+                 typeName='text/html',
+                 className='w3-card',
+                 **kwargs):
+        self.tag = tag
         self.typeName = typeName
         self.src = src
         self.width = width
         self.height = height
         self.params = kwargs
+        self.className = className
 
     def _repr_html_(self):
         """return the embed iframe"""
@@ -23,9 +30,39 @@ class Embed:
             params = "?" + urlencode(self.params)
         else:
             params = ""
-        return self.embed.format(
-            typeName=self.typeName,
-            src=self.src,
-            width=self.width,
-            height=self.height,
-            params=params)
+        config = {
+            'tag': self.tag,
+            'typeName': self.typeName,
+            'src': self.src,
+            'width': self.width,
+            'height': self.height,
+            'params': params,
+            'className': self.className
+        }
+        return null_element_template.substitute(config)
+
+
+def IFrame(src, height,
+           width='100%',
+           typeName='text/html',
+           className='w3-card',
+           **kwargs):
+    '''<iframe>'''
+    return FrameElement('iframe', src, height,
+                        width,
+                        typeName,
+                        className,
+                        **kwargs)
+
+
+def Embed(src, height,
+          width='100%',
+          typeName='text/html',
+          className='w3-card',
+          **kwargs):
+    '''<embed>'''
+    return FrameElement('embed', src, height,
+                        width,
+                        typeName,
+                        className,
+                        **kwargs)
